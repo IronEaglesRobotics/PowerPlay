abc38e5bb2bb9c29b7681f009df670fdd9c00e08
tealro <39542562+tealrex@users.noreply.github.com>
2022-09-22 16:12:55 -0500

    improved turtle code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedyMaybev2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedyMaybev2.java
@@ -23,20 +23,25 @@ public class SpeedyMaybev2 extends OpMode {
     double x;
     double z;
     int pivotPosition; // -2000 to 2000
+    int pivotCounter;
 
     double d1;//mid 0.61
     double d2;
 
-    public static double PIVOT_SPEED = 2;
-    public static double RETURN_SPEED = 1;
-    public static int PIVOT_LEFT_BOUNDARY = -50;
-    public static int PIVOT_RIGHT_BOUNDARY = 50;
-    public static double FRONT_SPEED = 1.0;
+    public static double PIVOT_DELAY = 5;
+    public static double TURN_SPEED = 4;
+    public static double RETURN_SPEED = 2;
+    public static int PIVOT_BOUNDARY_LEFT = -85;
+    public static int PIVOT_BOUNDARY_RIGHT = 85;
+    public static double FRONT_SPEED = 0.1;
     public static double BACK_SPEED = 0.5;
     public static double BACK_DIRECTION = -1.0;
-    public static double PIVOT_P = 0.03;
+    public static double PIVOT_P = 0.04;
     public static double PIVOT_I = 0;
-    public static double PIVOT_D = 0;
+    public static double PIVOT_D = 0.0001;
+    public static double OFFSET = 0;
+    public static double DEADZONE = 5;
+    public static double DIFFERENTIAL = 0.5;
 
     @Override
     public void init() {
@@ -49,8 +54,12 @@ public class SpeedyMaybev2 extends OpMode {
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         pivot.setDirection(DcMotorSimple.Direction.REVERSE);
         pivotPosition = 0;
+        pivotCounter = 0;
 
         pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -67,18 +76,24 @@ public class SpeedyMaybev2 extends OpMode {
 
 
         // turn based on input
-        pivotPosition += x * PIVOT_SPEED;
-        if (pivotPosition > 0) {
-            pivotPosition -= RETURN_SPEED;
-        } else {
-            pivotPosition += RETURN_SPEED;
+        pivotCounter++;
+        if (pivotCounter >= PIVOT_DELAY) {
+            pivotPosition += x * TURN_SPEED;
+            if (pivotPosition > DEADZONE) {
+//                pivotPosition -= ((pivotPosition+OFFSET)/PIVOT_BOUNDARY_RIGHT) * RETURN_SPEED;
+                pivotPosition -= RETURN_SPEED;
+            } else if (pivotPosition < -DEADZONE){
+//                pivotPosition += ((pivotPosition+OFFSET)/PIVOT_BOUNDARY_RIGHT) * RETURN_SPEED;
+                pivotPosition += RETURN_SPEED;
+            }
+            pivotCounter = 0;
         }
 
-        pivotPosition = Math.min(Math.max(PIVOT_LEFT_BOUNDARY, pivotPosition), PIVOT_RIGHT_BOUNDARY);
+        pivotPosition = Math.min(Math.max(PIVOT_BOUNDARY_LEFT, pivotPosition), PIVOT_BOUNDARY_RIGHT);
 
         // go to position
         controller.setPID(PIVOT_P, PIVOT_I, PIVOT_D);
-        controller.setSetPoint(pivotPosition);
+        controller.setSetPoint(pivotPosition+OFFSET);
         pivot.setPower(controller.calculate(pivot.getCurrentPosition()));
 //        if (pivot.getCurrentPosition() < pivotPosition) {
 //            pivot.setPower(0.2);
@@ -86,14 +101,17 @@ public class SpeedyMaybev2 extends OpMode {
 //            pivot.setPower(-0.2);
 //        }
 
+
+//        frontLeft.setPower(Math.copySign(Math.abs(y*FRONT_SPEED)+y*DIFFERENTIAL*((pivotPosition+OFFSET)/PIVOT_BOUNDARY_RIGHT), y));
+//        frontRight.setPower(Math.copySign(Math.abs(y*FRONT_SPEED)-y*DIFFERENTIAL*((pivotPosition+OFFSET)/PIVOT_BOUNDARY_RIGHT), y));
         frontLeft.setPower(y*FRONT_SPEED);
         frontRight.setPower(y*FRONT_SPEED);
 //        back.setPower(x*BACK_SPEED*BACK_DIRECTION);
-        back.setPower((pivotPosition/50.0)*BACK_SPEED*BACK_DIRECTION);
+        back.setPower(((pivotPosition+OFFSET)/PIVOT_BOUNDARY_RIGHT)*BACK_SPEED*BACK_DIRECTION);
 //        backRight.setPower(x*BACK_SPEED*BACK_DIRECTION);
 
 
-        telemetry.addLine("Pivot: "+ pivotPosition + " HI: " + pivot.getCurrentPosition());
+        telemetry.addLine("Pivot: "+ pivotPosition + " " + OFFSET + " " + pivot.getCurrentPosition());
         telemetry.update();
     }
 }
