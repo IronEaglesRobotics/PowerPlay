67bbd89833a9ee77056fa0ee648aa731620cf7b9
Justin <wydrajus@student.saviochs.org>
2022-09-27 17:49:03 -0500

    Added claw to xdrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XtremeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XtremeDrive.java
@@ -1,20 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
+@Config
 @TeleOp
 public class XtremeDrive extends OpMode {
     SampleMecanumDrive drive;
+    Servo leftClaw;
+    Servo rightClaw;
+    public static double leftOpen;
+    public static double leftClosed;
+    public static double rightOpen;
+    public static double rightClosed;
 
     @Override
     public void init() {
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(PoseStorage.currentPose);
+        leftClaw = hardwareMap.get(Servo.class,"leftClaw");
+        rightClaw = hardwareMap.get(Servo.class,"rightClaw");
+        leftOpen = 0;
+        leftClosed = 1;
+        rightOpen = 0;
+        rightClosed = 1;
     }
 
     @Override
@@ -22,12 +37,21 @@ public class XtremeDrive extends OpMode {
         double x = -gamepad1.left_stick_y;
         double y = -gamepad1.left_stick_x;
         double z = -gamepad1.right_stick_x;
+        boolean a = gamepad1.a;
 
         Pose2d poseEstimate = drive.getPoseEstimate();
         Vector2d input = new Vector2d(x,y).rotated(-poseEstimate.getHeading());
-        drive.setWeightedDrivePower(new Pose2d(input.getX(), input.getY(), z));
+        drive.setWeightedDrivePower(new Pose2d(input.getX(), input.getY(), z/2.0));
 //        drive.setWeightedDrivePower(new Pose2d(x, y, z));
         drive.update();
+
+        if (a) {
+            leftClaw.setPosition(leftClosed);
+            rightClaw.setPosition(rightClosed);
+        } else {
+            leftClaw.setPosition(leftOpen);
+            rightClaw.setPosition(rightOpen);
+        }
 
         telemetry.addData("x", poseEstimate.getX());
         telemetry.addData("y", poseEstimate.getY());
