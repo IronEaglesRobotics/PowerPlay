cd2207ce97fe1858794379efadadf5d6751d3bb4
Evan Kuo <evan.kuo.edu@gmail.com>
2022-10-05 18:44:22 -0500

    made reverse true in curved testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurvedTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurvedTesting.java
@@ -24,35 +24,21 @@ public class CurvedTesting extends LinearOpMode {
         Pose2d simpleJ2 = new Pose2d(-12,6,Math.toRadians(45));
         Pose2d simpleJ3 = new Pose2d(-12,-18,Math.toRadians(-45));
 
-        // turret paths
-//        Trajectory t1 = drive.trajectoryBuilder(start).lineToLinearHeading(turretJ1).build();
-//        Trajectory t2 = drive.trajectoryBuilder(turretJ1).lineToLinearHeading(back).build();
-//        Trajectory t3 = drive.trajectoryBuilder(back).lineToLinearHeading(turretJ2).build();
-//        Trajectory t4 = drive.trajectoryBuilder(turretJ2).lineToLinearHeading(back).build();
-//        Trajectory t5 = drive.trajectoryBuilder(back).lineToLinearHeading(turretJ3).build();
-//        Trajectory t6 = drive.trajectoryBuilder(turretJ3).lineToLinearHeading(back).build();
-//
-//        Trajectory t7 = drive.trajectoryBuilder(back).lineToLinearHeading(turretJ1).build();
-//        Trajectory t8 = drive.trajectoryBuilder(turretJ1).lineToLinearHeading(back).build();
-//        Trajectory t9 = drive.trajectoryBuilder(back).lineToLinearHeading(turretJ2).build();
-//        Trajectory t10 = drive.trajectoryBuilder(turretJ2).lineToLinearHeading(back).build();
-//        Trajectory t11 = drive.trajectoryBuilder(back).lineToLinearHeading(turretJ3).build();
-//        Trajectory t12 = drive.trajectoryBuilder(turretJ3).lineToLinearHeading(back).build();
 
         // simple paths
         Trajectory t1 = drive.trajectoryBuilder(start).splineTo(new Vector2d(simpleJ1.getX(), simpleJ1.getY()), simpleJ1.getHeading()).build();
-        Trajectory t2 = drive.trajectoryBuilder(simpleJ1).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t2 = drive.trajectoryBuilder(simpleJ1, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
         Trajectory t3 = drive.trajectoryBuilder(back).splineTo(new Vector2d(simpleJ2.getX(), simpleJ2.getY()), simpleJ2.getHeading()).build();
-        Trajectory t4 = drive.trajectoryBuilder(simpleJ2).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t4 = drive.trajectoryBuilder(simpleJ2, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
         Trajectory t5 = drive.trajectoryBuilder(back).splineTo(new Vector2d(simpleJ3.getX(), simpleJ3.getY()), simpleJ3.getHeading()).build();
-        Trajectory t6 = drive.trajectoryBuilder(simpleJ3).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t6 = drive.trajectoryBuilder(simpleJ3, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
 
         Trajectory t7 = drive.trajectoryBuilder(back).splineTo(new Vector2d(simpleJ1.getX(), simpleJ1.getY()), simpleJ1.getHeading()).build();
-        Trajectory t8 = drive.trajectoryBuilder(simpleJ1).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t8 = drive.trajectoryBuilder(simpleJ1, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
         Trajectory t9 = drive.trajectoryBuilder(back).splineTo(new Vector2d(simpleJ2.getX(), simpleJ2.getY()), simpleJ2.getHeading()).build();
-        Trajectory t10 = drive.trajectoryBuilder(simpleJ2).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t10 = drive.trajectoryBuilder(simpleJ2, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
         Trajectory t11 = drive.trajectoryBuilder(back).splineTo(new Vector2d(simpleJ3.getX(), simpleJ3.getY()), simpleJ3.getHeading()).build();
-        Trajectory t12 = drive.trajectoryBuilder(simpleJ3).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
+        Trajectory t12 = drive.trajectoryBuilder(simpleJ3, true).splineTo(new Vector2d(back.getX(), back.getY()), back.getHeading()).build();
 
         drive.setPoseEstimate(start);//12.5x12  11.25
 
