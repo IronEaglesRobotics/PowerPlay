652ecb0473b24b6f9369b023a18dd0708dfb0646
Scott Barnes <scott@tearabite.com>
2023-04-05 10:46:25 -0500

    Initial WalE test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -87,7 +87,7 @@ public class SampleMecanumDrive extends MecanumDrive {
         super(kV, kA, kStatic, TRACK_WIDTH, TRACK_WIDTH, LATERAL_MULTIPLIER);
 
         follower = new HolonomicPIDVAFollower(TRANSLATIONAL_PID, TRANSLATIONAL_PID, HEADING_PID,
-                new Pose2d(0.001, 0.001, Math.toRadians(0.1)), 0.5);
+                new Pose2d(0.1, 0.1, Math.toRadians(0.1)), 0.1);
 
         LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WaleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/WaleTest.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import android.annotation.SuppressLint;
+
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,16 +16,26 @@ import org.firstinspires.ftc.teamcode.hardware.Robot;
 @Autonomous(name = "WALE", group = "Competition", preselectTeleOp = "MainTeleOp")
 public class WaleTest extends LinearOpMode {
     Robot robot;
-    private final double targetDistance = 4.0;
-    private final double tolerance = 0.1;
+    public static double targetDistance = 4.5;
+    public static double tolerance = 0.1;
     public static double speed = 0.2;
 
+    public static double backoff = 0.25;
+
+    @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         this.robot = new Robot().init(hardwareMap);
         this.robot.getClaw().openWide();
 
         while(!isStarted()) {
+            this.robot.getDrive().update();
+
+            Pose2d poseEstimate = this.robot.getDrive().getPoseEstimate();
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
             telemetry.addData("distance", this.robot.getWale().getDistance(DistanceUnit.INCH));
             telemetry.addData("touch", this.robot.getWale().isPressed());
             telemetry.update();
@@ -32,36 +46,36 @@ public class WaleTest extends LinearOpMode {
         while(!this.robot.getWale().isPressed() && opModeIsActive()) {
             this.robot.getDrive().setWeightedDrivePower(new Pose2d(speed ,0, 0));
         }
-        this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(new Pose2d()).back(2.0).build());
-
-        double currentDistance = Double.MAX_VALUE;
-        for (int i = 0; i < 10; i++) {
-            double d = robot.getWale().getDistance(DistanceUnit.INCH);
-            if (d < currentDistance) {
-                currentDistance = d;
+        double runningTotal = 0;
+        int i = 0;
+        int samples = 10;
+        while (i < samples) {
+            double d = this.robot.getWale().getDistance(DistanceUnit.INCH);
+            if (d > 20.0) {
+                continue;
             }
+            runningTotal += this.robot.getWale().getDistance(DistanceUnit.INCH);
+            i++;
         }
+        double currentDistance = runningTotal / samples;
         double error = Math.abs(targetDistance - currentDistance);
 
-        if (currentDistance > targetDistance) {
-            telemetry.addLine("Moving Left");
+        this.robot.getDrive().update();
+        this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(this.robot.getDrive().getPoseEstimate()).back(backoff).build());
+
+        telemetry.addLine(String.format("PoseBeforeMove: %s", this.robot.getDrive().getPoseEstimate()));
+        if (currentDistance > targetDistance + tolerance) {
+            telemetry.addLine(String.format("Moving Left %f inches", error));
             telemetry.addData("currentDistance", currentDistance);
-            telemetry.update();
-            this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(new Pose2d()).strafeLeft(error).build());
-        } else if (currentDistance < targetDistance) {
-            telemetry.addLine("Moving Right");
+            this.robot.getDrive().update();
+            this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(this.robot.getDrive().getPoseEstimate()).strafeLeft(error).build());
+        } else if (currentDistance < targetDistance - tolerance) {
+            telemetry.addLine(String.format("Moving Right %f inches", error));
             telemetry.addData("currentDistance", currentDistance);
-            telemetry.update();
-            this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(new Pose2d()).strafeRight(error).build());
+            this.robot.getDrive().update();
+            this.robot.getDrive().followTrajectory(this.robot.getDrive().trajectoryBuilder(this.robot.getDrive().getPoseEstimate()).strafeRight(error).build());
         }
-//
-//        while(opModeIsActive()) {
-//            currentDistance = this.robot.getWale().getDistance(DistanceUnit.INCH);
-//            error = targetDistance - currentDistance;
-//
-//            telemetry.addData("distance", currentDistance);
-//            telemetry.addData("error", error);
-//            telemetry.update();
-//        }
+        telemetry.addLine(String.format("PoseAfterMove: %s", this.robot.getDrive().getPoseEstimate()));
+        telemetry.update();
     }
 }
